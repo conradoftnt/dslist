74554c2f99f4088996890e8ca3e49ba2494e0a18
Conrado Fortunato <conradofortunato246@gmail.com>
2025-01-21 19:45:51 -0300

    Domain model + Database seed update

---
+++ src/main/java/com/conradofortunato/dslist/entities/BelongingPK.java
@@ -0,0 +1,68 @@
+package com.conradofortunato.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class BelongingPK {
+
+    @ManyToOne
+    @JoinColumn(name="game_id")
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name="list_id")
+    private GameList gameList;
+
+    public BelongingPK(){};
+
+    public BelongingPK(Game game, GameList gameList) {
+        this.game = game;
+        this.gameList = gameList;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public GameList getGameList() {
+        return gameList;
+    }
+
+    public void setGameList(GameList gameList) {
+        this.gameList = gameList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + Objects.hashCode(this.game);
+        hash = 59 * hash + Objects.hashCode(this.gameList);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final BelongingPK other = (BelongingPK) obj;
+        if (!Objects.equals(this.game, other.game)) {
+            return false;
+        }
+        return Objects.equals(this.gameList, other.gameList);
+    }
+}
