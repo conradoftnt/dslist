0785f59800ceea7481a89d2e95601a9ccf6d8695
Conrado Fortunato <conradofortunato246@gmail.com>
2025-01-21 20:09:52 -0300

    Get game by id

--- src/main/java/com/conradofortunato/dslist/controllers/GameController.java
+++ src/main/java/com/conradofortunato/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.conradofortunato.dslist.dto.GameDTO;
 import com.conradofortunato.dslist.dto.GameMinDTO;
 import com.conradofortunato.dslist.services.GameService;
 
@@ -23,4 +25,9 @@ public class GameController {
         return result;
     }
 
+    @GetMapping(value="/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 }
